4d25f7ff46f49c852f499a068e808f556f9e1dbe
Rishabh Khevaria <rishabh.khevaria@smu.ca>
2023-12-03 22:16:57 -0400

    Pushing Core MVC Structure

---
+++ Models/CarDetails.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace swift_car_rental.Models { 
+    public class CarDetails
+    {
+        public int Id { get; set; }
+        public string Model { get; set; }
+
+        public int ModelYear { get; set; }
+
+        public int Odometer { get; set; }
+        public int NoOfSeats { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Price { get; set; }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace swift_car_rental.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
